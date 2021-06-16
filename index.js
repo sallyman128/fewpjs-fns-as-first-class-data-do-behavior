25c1f807c1d4af8180720b5b4b2eadbceb533409
Salmaan <salmaan.arif.ali@gmail.com>
2021-06-16 01:30:26 -0400

    commit tree eacf5aaa5120ce19ae1744a5514a4081cd7cdd03

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+
+function greet(time) {
+  let hour = time.split(":")[0];
+  hour = parseInt(hour);
+
+  if (hour < 12) {
+    return "Good Morning";
+  } else if (hour < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
+function displayMessage(newContent) {
+  document.getElementById('greeting').innerText = newContent;
+}
